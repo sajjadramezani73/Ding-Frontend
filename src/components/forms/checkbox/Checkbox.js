c4cd0ed7cf64df9d6abeb88e0024f99b82408f1d
sajjadramezani <sajjad.payam77@gmail.com>
2022-11-17 18:11:14 +0330

    feat: Add form element conponent
    input and checkbox components

--- src/components/forms/checkbox/Checkbox.js
+++ src/components/forms/checkbox/Checkbox.js
@@ -1,13 +1,13 @@
 import LoadIconSvg from '../../../utils/LoadSvgIcon'
 
-const Checkbox = ({ title, checked, onClick }) => {
+const Checkbox = ({ title, checked, onClick, theme = 'dark' }) => {
     return (
         <div className='flex items-center cursor-pointer ml-4' onClick={onClick}>
             <span className={`flex justify-center items-center w-4 h-4 border-2 rounded 
             ${checked ? 'bg-primary border-primary' : 'border-caption'}`}>
                 {checked ? <LoadIconSvg name='check' color='#fff' weight={2} size={15} /> : null}
             </span>
-            <p className={`mr-2 text-sm ${checked ? 'text-primary' : 'text-gray-500'}`}>{title}</p>
+            <p className={`mr-2 text-sm ${checked ? (theme === 'light' ? 'text-white' : 'text-primary') : 'text-gray-500'}`}>{title}</p>
         </div>
     )
 }
--- src/components/forms/textInput/Input.js
+++ src/components/forms/textInput/Input.js
@@ -9,16 +9,11 @@ const Input = ({
     rule = "",
     value = "",
     onChange,
-    errorMessage = null,
     attributes = {},
-    haveError = function () { },
-
 }) => {
 
     const [focused, setFocused] = useState(false);
     const [inputValue, setInputValue] = useState('')
-    const [error, setError] = useState(null)
-    const clearInputValue = () => onChangeHandler('')
 
     useEffect(() => {
         setInputValue(value)
@@ -32,61 +27,40 @@ const Input = ({
         })
     }
 
-    useEffect(() => {
-        if (value !== '' && value?.length >= 0) {
-            if (error) {
-                haveError(true)
-            } else {
-                haveError(false)
-            }
-        } else {
-            haveError(true)
-        }
-    }, [error, value])
-
     return (
-        <div className="py-2">
-            <div className={`flex h-11 rounded-xl border items-center
-            ${error != null ? 'border-danger' : focused ? 'border-gray-500' : (!!inputValue ? 'border-body' : '')}`}>
-                <div className='w-10 h-full flex justify-center items-center'>
-                    <LoadSvgIcon name={iconName} size="16" />
+        <>
+            <div className={`flex items-center h-10 rounded-3xl bg-white`}>
+                <div className='w-10 h-full mr-2 flex justify-center items-center'>
+                    <LoadSvgIcon name={iconName} size="16" weight={1.5} color="var(--color-captionLight)" />
                 </div>
-                <div className='grow h-full relative text-right'>
+                <div className='flex-grow h-full relative'>
                     <input
                         type={type}
                         value={inputValue}
-                        onChange={val => onChangeHandler(val.target.value)}
+                        onChange={e => onChangeHandler(e.target.value)}
                         onFocus={() => setFocused(true)}
                         onBlur={() => setFocused(false)}
                         className="w-full h-full outline-none bg-transparent text-xxs text-body font-bold componentinput"
                         placeholder={placeholder}
                         {...attributes} />
                 </div>
-                {value != '' ?
-                    <span className="px-2" onClick={clearInputValue}>
-                        <LoadSvgIcon name="close-circle" size={16} />
+                <div className="w-10 min-w-[40px] h-full flex items-center justify-center">
+                    <span className='w-4 h-4 rounded-full bg-green-500 flex items-center justify-center'>
+                        <LoadSvgIcon name="check" size={13} color="#ffffff" />
                     </span>
-                    : null}
-            </div>
-            {error != null ?
-                <div className="w-full flex mt-2">
-                    <span className="px-2">
-                        <LoadSvgIcon name="info" color="var(--color-danger)" size={16} />
-                    </span>
-                    <span className="text-danger text-[10px]">{error}</span>
                 </div>
-                : null
-            }
+            </div>
+            
             <style>
                 {`
                     .componentinput::placeholder {
-                        color:#c1c1c1;
-                        font-size:12px;
-                        font-weight:500;
+                        color:var(--color-captionLight);
+                        font-size:11px;
+                        font-weight:400;
                     }
                 `}
             </style>
-        </div>
+        </>
     );
 };
 
