1efdaaf88bffba4474068fea90b19db7ce89c997
sajjadramezani <62692232+sajjadramezani73@users.noreply.github.com>
2022-11-28 22:01:33 +0330

    style: Change style button

--- src/components/ui/button/Button.js
+++ src/components/ui/button/Button.js
@@ -1,17 +1,15 @@
 import React from 'react'
 import LoadSvgIcon from '../../../utils/LoadSvgIcon'
 
-const Button = ({ title, width = '', onClick, loading, disabled, className }) => {
+const Button = ({ title, onClick, loading, disabled, className }) => {
     return (
         <button
-            className={`font-bold text-xxs rounded-3xl h-10 flex items-center justify-center bg-primary text-white
-            ${(loading || disabled ) && 'bg-primary/[0.7]'}
-            ${width}
+            className={`font-bold text-xxs rounded-md h-10 flex items-center justify-center bg-primary text-white
             ${className}`}
             disabled={loading || disabled}
             onClick={onClick}
         >
-            {loading ? <LoadSvgIcon name="loading" fill='#ffffff' color="#ffffff" /> : title}
+            {loading ? <LoadSvgIcon name="loading" fill='#ffffff' size={20} color="#ffffff" /> : title}
         </button >
     )
 }
