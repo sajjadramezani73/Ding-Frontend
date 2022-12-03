2191faf88407d8587ee89e74a66630eab35a8a0e
sajjadramezani <sajjad.payam77@gmail.com>
2022-12-03 10:12:44 +0330

    refactor: Complate portal modal and sidebar ui
    add transition for modal and overlay

--- src/containers/sidebar/Sidebar.js
+++ src/containers/sidebar/Sidebar.js
@@ -4,9 +4,11 @@ import ModalRoot from '../../components/modals/Modal-root'
 const Sidebar = ({ show, setShow }) => {
     return (
         <ModalRoot>
-            <div className={`fixed top-0 left-1/2 w-full max-w-md h-full bg-black/[0.5] backdrop-blur-sm -translate-x-1/2 overflow-hidden overlay ${show && 'active'}`}
+            <div className={`fixed top-0 left-1/2 w-full max-w-md h-full bg-black/[0.5] backdrop-blur-sm -translate-x-1/2 overflow-hidden duration-200 overlay 
+            ${show ? 'opacity-100 visible' : 'opacity-0 invisible delay-300'}`}
                 onClick={(e) => (e.target === e.currentTarget && setShow(false))}>
-                <div className={`bg-white h-full w-4/5 max-w-[320px] translate-x-full sidebar ${show && 'active'}`}></div>
+                <div className={`bg-white h-full w-4/5 max-w-[320px]  duration-300 sidebar 
+                ${show ? 'translate-x-0 delay-200' : 'translate-x-full'}`}></div>
             </div>
         </ModalRoot>
     )
