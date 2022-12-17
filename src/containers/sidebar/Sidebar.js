25a91da0d0d755ce05e2948f209344e44a1587b5
sajjadramezani <sajjad.payam77@gmail.com>
2022-12-17 21:05:11 +0330

    fix: Add z-index to sidebar

--- src/containers/sidebar/Sidebar.js
+++ src/containers/sidebar/Sidebar.js
@@ -20,7 +20,7 @@ const Sidebar = ({ show, setShow }) => {
 
     return (
         <ModalRoot>
-            <div className={`fixed top-0 left-1/2 w-full max-w-md h-full bg-black/[0.5] backdrop-blur-sm -translate-x-1/2 overflow-hidden duration-200 overlay 
+            <div className={`fixed top-0 left-1/2 z-10 w-full max-w-md h-full bg-black/[0.5] backdrop-blur-sm -translate-x-1/2 overflow-hidden duration-200 overlay 
             ${show ? 'opacity-100 visible' : 'opacity-0 invisible delay-300'}`}
                 onClick={(e) => (e.target === e.currentTarget && setShow(false))}>
                 <div className={`bg-white h-full w-4/5 max-w-[320px]  duration-300 sidebar 
