8cc2f01ae5144371cf5e931f4d2500da52b71342
sajjadramezani <sajjad.payam77@gmail.com>
2022-12-27 14:53:41 +0330

    feat: Create full modal and fix bug modal-root and sidebar

--- src/components/modals/Modal-root.js
+++ src/components/modals/Modal-root.js
@@ -1,7 +1,15 @@
 import { createPortal } from 'react-dom';
 
-const ModalRoot = ({ children }) => {
-    return createPortal(children, document.querySelector("#modal-root"));
+const ModalRoot = ({ children, show, onClick }) => {
+    return createPortal((
+        <div
+            className={`fixed top-0 left-1/2 z-10 w-full max-w-md h-full bg-black/[0.5] backdrop-blur-sm -translate-x-1/2 overflow-hidden duration-200
+            ${show ? 'opacity-100 visible ' : 'opacity-0 invisible delay-300'}`}
+            onClick={(e) => (e.target === e.currentTarget && onClick(false))}
+        >
+            {children}
+        </div>
+    ), document.querySelector("#modal-root"));
 }
 
 export default ModalRoot
--- src/containers/sidebar/Sidebar.js
+++ src/containers/sidebar/Sidebar.js
@@ -19,30 +19,25 @@ const Sidebar = ({ show, setShow }) => {
     }
 
     return (
-        <ModalRoot>
-            <div className={`fixed top-0 left-1/2 z-10 w-full max-w-md h-full bg-black/[0.5] backdrop-blur-sm -translate-x-1/2 overflow-hidden duration-200 overlay 
-            ${show ? 'opacity-100 visible' : 'opacity-0 invisible delay-300'}`}
-                onClick={(e) => (e.target === e.currentTarget && setShow(false))}>
-                <div className={`bg-white h-full w-4/5 max-w-[320px]  duration-300 sidebar 
+        <ModalRoot show={show} onClick={value => setShow(value)}>
+            <div className={`bg-white h-full w-4/5 max-w-[320px]  duration-300 sidebar 
                 ${show ? 'translate-x-0 delay-200' : 'translate-x-full'}`}>
-                    <div className="h-full">
-                        <div className="bg-primary flex flex-col justify-center items-center py-4">
-                            <img src={avatar(user)} alt="logo" className='w-20 h-20 rounded-full object-cover' />
-                            <p className='text-tiny text-white mt-2'>{user?.firstName + ' ' + user?.lastName}</p>
+                <div className="h-full">
+                    <div className="bg-primary flex flex-col justify-center items-center py-4">
+                        <img src={avatar(user)} alt="logo" className='w-20 h-20 rounded-full object-cover' />
+                        <p className='text-tiny text-white mt-2'>{user?.firstName + ' ' + user?.lastName}</p>
+                    </div>
+                    <div className="p-4 pt-3">
+                        <div className='border-b mb-4'>
+                            <SidebarLinks />
                         </div>
-                        <div className="p-4 pt-3">
-                            <div className='border-b mb-4'>
-                                <SidebarLinks />
-                            </div>
-                            <div className='px-10'>
-                                {/* <button className='w-full h-10 rounded-lg bg-primary text-white text-sm'>خروج</button> */}
-                                <Button
-                                    title="خروج"
-                                    // disabled={true}
-                                    // loading={true}
-                                    onClick={() => console.log('log out')}
-                                />
-                            </div>
+                        <div className='px-10'>
+                            <Button
+                                title="خروج"
+                                // disabled={true}
+                                // loading={true}
+                                onClick={() => console.log('log out')}
+                            />
                         </div>
                     </div>
                 </div>
