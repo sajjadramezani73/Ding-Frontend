3cd8e5b7479d521f3030d493e8506e2e170b9ba3
sajjadramezani <sajjad.payam77@gmail.com>
2022-12-29 15:15:12 +0330

    fix: Add error permision to useGetUserLocation
    show error and loading for map container

--- src/containers/mapContainer/MapContainer.js
+++ src/containers/mapContainer/MapContainer.js
@@ -1,15 +1,18 @@
 import { useEffect } from 'react'
 import { useDispatch, useSelector } from 'react-redux'
 import Map from '../../components/map/Map'
+import FullModal from '../../components/modals/FullModal'
+import Button from '../../components/ui/button/Button'
 import useGetUserLocation from '../../hooks/useGetUserLocation'
 import { addMapLocation } from '../../store/mapSlice'
+import LoadSvgIcon from '../../utils/LoadSvgIcon'
 
 const MapContainer = () => {
 
     const dispatch = useDispatch()
     const { map } = useSelector(store => store.map);
 
-    const { loading, setLoading, getPosition, setLatLng, locationData } = useGetUserLocation()
+    const { loading, locationError, setLocationError, getPosition, setLatLng, locationData } = useGetUserLocation()
 
     useEffect(() => {
         storeLatLng(locationData)
@@ -21,19 +24,45 @@ const MapContainer = () => {
     }
 
     return (
-        <div className="w-full h-full flex justify-center items-center relative">
-            <Map
-                zoom={14}
-                position={{
-                    lat: locationData?.lat,
-                    lng: locationData?.lng
+        <>
+            <div className="w-full h-full flex justify-center items-center relative">
+                <Map
+                    zoom={14}
+                    position={{
+                        lat: locationData?.lat,
+                        lng: locationData?.lng
+                    }}
+                    width="100%"
+                    height="100%"
+                    getLatLng={value => storeLatLng(value)}
+                    getPosition={() => getPosition()}
+                />
+            </div>
+
+            <FullModal
+                options={{
+                    show: loading || locationError?.showError,
+                    // setShow: () => setResultModal(false),
                 }}
-                width="100%"
-                height="100%"
-                getLatLng={value => storeLatLng(value)}
-                getPosition={() => getPosition()}
-            />
-        </div>
+            >
+                <div className="p-5 min-h-[164px] bg-white flex justify-center items-center">
+                    {loading && <LoadSvgIcon name="loading" fill="var(--color-caption)" />}
+                    {locationError?.showError && (
+                        <div className="flex flex-col items-center">
+                            <LoadSvgIcon name="resultDanger" color="var(--color-danger)" size={35} />
+                            <p className='text-sm text-captionDark py-4 text-center'>{locationError?.messageError}</p>
+                            <div className="">
+                                <Button
+                                    title="متوجه شدم"
+                                    className="h-9 text-xxs px-3"
+                                    onClick={() => setLocationError({ showError: false, messageError: '' })}
+                                />
+                            </div>
+                        </div>
+                    )}
+                </div>
+            </FullModal>
+        </>
     )
 }
 
--- src/hooks/useGetUserLocation.js
+++ src/hooks/useGetUserLocation.js
@@ -2,48 +2,52 @@ import { useState } from "react";
 
 const useGetUserLocation = () => {
 
-    const [loading, setLoading] = useState(true);
+    const [loading, setLoading] = useState(false);
     const [latlng, setLatLng] = useState({ lat: 35.688180, lng: 51.392918, zoom: 14 });
+    const [locationError, setLocationError] = useState({
+        showError: false,
+        messageError: ''
+    });
 
-    const getPosition = async () => {
+    const getPosition = async (e) => {
+        setLoading(true)
         if (navigator.geolocation) {
             await navigator.geolocation.getCurrentPosition((position) => {
                 setLatLng({
                     lat: position.coords.latitude,
                     lng: position.coords.longitude,
                 });
+                setLoading(false)
             }, showError);
         }
     };
+
     const showError = (error) => {
+        setLoading(false)
         switch (error.code) {
             case error.PERMISSION_DENIED:
-                // setLocationError({
-                //     hasError: true,
-                //     msg: "در بخش تنظیمات تلفن‌ همراه‌تان دسترسی موقعیت مکانی را فعال کنید.",
-                // });
-                console.log('در بخش تنظیمات تلفن‌ همراه‌تان دسترسی موقعیت مکانی را فعال کنید.')
+                setLocationError({
+                    showError: true,
+                    messageError: "در بخش تنظیمات تلفن‌ همراه‌، دسترسی موقعیت مکانی را فعال کنید."
+                })
                 break;
             case error.POSITION_UNAVAILABLE:
-                // setLocationError({
-                //     hasError: true,
-                //     msg: "اطلاعات موقعیت مکانی در دسترس نمیباشد",
-                // });
-                console.log("اطلاعات موقعیت مکانی در دسترس نمیباشد");
+                setLocationError({
+                    showError: true,
+                    messageError: "اطلاعات موقعیت مکانی در دسترس نمی باشد."
+                })
                 break;
             case error.TIMEOUT:
-                // setLocationError({
-                //     hasError: true,
-                //     msg: "اطلاعات دریافت نشد ، مجددا تلاش نمایید",
-                // });
-                console.log("اطلاعات دریافت نشد ، مجددا تلاش نمایید")
+                setLocationError({
+                    showError: true,
+                    messageError: "اطلاعات دریافت نشد ، مجددا تلاش نمایید."
+                })
                 break;
             case error.UNKNOWN_ERROR:
-                // setLocationError({
-                //     hasError: true,
-                //     msg: "خطلای غیرمنتظره ای رخ داده است ، مجدد تلاش نمایید",
-                // });
-                console.log("خطلای غیرمنتظره ای رخ داده است ، مجدد تلاش نمایید");
+                setLocationError({
+                    showError: true,
+                    messageError: "خطلای غیرمنتظره ای رخ داده است ، مجدد تلاش نمایید."
+                })
                 break;
             default:
                 console.log('default');
@@ -55,6 +59,8 @@ const useGetUserLocation = () => {
         getPosition,
         setLatLng,
         locationData: latlng,
+        locationError,
+        setLocationError
     };
 };
 
