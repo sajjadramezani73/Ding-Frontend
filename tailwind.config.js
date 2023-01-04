b3d88b8cdf92cedcf08869a58ec272c2ec94cec3
sajjadramezani <sajjad.payam77@gmail.com>
2023-01-04 19:40:02 +0330

    fix: Add shadow bottom to tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -57,6 +57,7 @@ module.exports = {
       boxShadow: {
         card: '0px 0px 10px rgba(90, 75, 75, 0.1), 0px 1px 1px rgba(90, 75, 75, 0.03), 0px 2px 3px rgba(90, 75, 75, 0.04)',
         top: '0 -4px 6px -1px rgb(0 0 0/0.1),0 2px 4px -2px rgb(0 0 0/0.1)',
+        bottom: '0px 1px 7px -4px rgba(0, 0, 0, 0.48)'
       },
     },
   },
