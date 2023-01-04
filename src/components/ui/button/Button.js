c7e6d5861ae93a7499a1b9370250399de94dd6c1
sajjadramezani <sajjad.payam77@gmail.com>
2023-01-04 19:39:40 +0330

    fix: Add outline props to Button component

--- src/components/ui/button/Button.js
+++ src/components/ui/button/Button.js
@@ -1,9 +1,10 @@
 import LoadSvgIcon from '../../../utils/LoadSvgIcon'
 
-const Button = ({ title, onClick, loading, disabled, className }) => {
+const Button = ({ title, onClick, loading, disabled, className, outline = false }) => {
     return (
         <button
-            className={`w-full text-sm rounded-lg h-10 flex items-center justify-center bg-primary text-white
+            className={`w-full text-sm rounded-lg h-10 flex items-center justify-center
+            ${outline ? 'border border-primary text-primary' : 'bg-primary text-white'}
             ${className}`}
             disabled={loading || disabled}
             onClick={onClick}
