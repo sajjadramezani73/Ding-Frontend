756bc25b633b187ceceb37cdffccacde4adae621
sajjadramezani <sajjad.payam77@gmail.com>
2023-01-15 14:57:39 +0330

    fix: Show total_working_hours in summary reports

--- src/containers/reports/SummaryReports.js
+++ src/containers/reports/SummaryReports.js
@@ -1,7 +1,15 @@
-import React from 'react'
+import moment from 'jalali-moment'
+import React, { useEffect } from 'react'
 import LoadSvgIcon from '../../utils/LoadSvgIcon'
 
 const SummaryReports = ({ loading, reports }) => {
+
+    // remove second from time
+    const RemoveSecondFromTime = (time) => {
+        let m = moment.from(time, 'fa', 'HH:mm:ss')
+        return m.locale('fa').format('HH:mm')
+    }
+
     return (
         loading ? (
             <div className='h-full flex justify-center items-center'>
@@ -15,7 +23,7 @@ const SummaryReports = ({ loading, reports }) => {
                 </div>
                 <div className="flex items-center justify-between py-2.5">
                     <p>مجموع ساعات کارکرد:</p>
-                    <p>00:00</p>
+                    <p>{RemoveSecondFromTime(reports?.total_working_hours)}</p>
                 </div>
                 <div className="flex items-center justify-between py-2.5">
                     <p>مجموع ساعات تاخیر در ورود:</p>
@@ -27,7 +35,7 @@ const SummaryReports = ({ loading, reports }) => {
                 </div>
                 <div className="flex items-center justify-between py-2.5">
                     <p>مجموع روزهای مرخصی:</p>
-                    <p>00:00</p>
+                    <p>0</p>
                 </div>
                 <div></div>
             </div>
